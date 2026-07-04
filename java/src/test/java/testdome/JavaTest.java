6cc124e6a4db63849318e4e80c7e3f952dcf8c3b
pham van lam <phamvanlam1991@gmail.com>
2026-07-04 17:44:18 +0700

    init java maven project

---
+++ java/src/test/java/testdome/JavaTest.java
@@ -0,0 +1,17 @@
+package testdome;
+
+import org.junit.Test;
+
+public class JavaTest {
+
+    public JavaTest() {
+    }
+
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Java.main(args);
+    }
+
+}
